8aa1b8255c23191aad881aba9607fbe1830204e2
Hansel Carmona <hcarmona.durango@udea.edu.co>
2026-09-17 09:56:20 -0500

    Add method readLine at CSVFileManagerControl.

--- src/main/java/com/mycompany/datamanagement/control/CSVFileManagerControl.java
+++ src/main/java/com/mycompany/datamanagement/control/CSVFileManagerControl.java
@@ -1,11 +1,29 @@
 package com.mycompany.datamanagement.control;
 
+import java.util.ArrayList;
+import java.io.*;
 /**
  *
  * @author Hansel
  */
 public class CSVFileManagerControl {
     
-    
+    //Define path to the CSV
+    public ArrayList<String> readLine(String ruta) throws IOException {
+        ArrayList<String> lineas = new ArrayList<>();
+        BufferedReader inputStream = null;
+        try {
+            inputStream = new BufferedReader(new FileReader(ruta));
+            String l;
+            while ((l = inputStream.readLine()) != null) {
+                lineas.add(l);
+            }
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+        return lineas;
+    }
     
 }
